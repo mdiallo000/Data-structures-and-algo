332938f3c316ed7017e67c5d28fb521f0714a2dc
mdiallo98 <mamadou1237@gmail.com>
2022-02-11 02:56:27 -0500

    added POP method

--- StaclQueu.js
+++ StaclQueu.js
@@ -4,6 +4,20 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+  Push(value) {
+    let newnode = new Node(value);
+    if (this.size == 0) {
+      this.first = newnode;
+      this.last - newnode;
+    }
+
+    if (this.size >= 1) {
+      let save = this.first;
+      this.first = newnode;
+      newnode.next = save;
+      this.size++;
+    }
+  }
 }
 
 class Node {
