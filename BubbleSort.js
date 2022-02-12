e535b3bab6dd5a55a8c7c684febd16d345a57f3b
mdiallo98 <mamadou1237@gmail.com>
2022-02-12 04:21:31 -0500

    Optimezed Bubble Sort

--- BubbleSort.js
+++ BubbleSort.js
@@ -5,18 +5,29 @@
 // *  arr[1] = temp
 // ! Another way of looking at it
 // ? Waiter2 is holding something but he wants to exchange his plate with 1, the plates are heavy so they cannot exchange betwenn one another. So waiter 2 gives his plate to the chef
-// **  chef = wiater2[chicken];
+// **  chef = waiter2[chicken];
 // ? Now with his empty hands waiter two can now take whatever waiter one is holdding
 // ** waiter2 = waiter1[lamb];
 // ? waiter 1 now takes what waiter 2 gave to the chef
 // ** waiter1 = Chef
 // ? The swap is finished they are both holding whatever the other was initinally
+// ==========================================================================================================
+
+// ==========================================================================================================
+
 let list = [23, 2, 45, 64, 231, 31];
 function BubbleSort(arr) {
-  for (let i = arr.length; i >= 0; i--) {
-    console.log(arr);
+  // =================================================================================================
+  //? What is the point the point of bubble sort
+  //* Bubble Sort is a sorting that cause the biggest number to get pushed at the back of the list
+  //* The result is a list where the items are sorted from least to greatest
+  //? How does this get achieved?
+  //*  So we compare the numbers as we go check each time if the previous number is greater than the next number
+  //*  If it is we swap.
+  // =================================================================================================
 
-    for (let j = i; j < i - 1; j++)
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++)
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -26,4 +37,25 @@ function BubbleSort(arr) {
   return arr;
 }
 
-console.log(BubbleSort(list));
+//*  More optimized way of doing Bubble sort is to include a mechanism that will check wheter or not we have a made a swap, if we have then it remains true, if we didnt make a previous swap then we need to brake out of our loop rather than continously looping for no reason. This variable essentially helps us stop the looop if we already finished sorting.
+
+function OptimalBubbleSort(arr) {
+  let DidWeSwap;
+  for (let i = arr.length; i > 0; i--) {
+    // ** at the start of the loop, DidWeSwap will start true, if we do complete a swap we turn the variable to false so that once it reaches the IF condition then we wont break out. But if we fail to swap then once we reach if(DidWeSwap) it will be true and we can breaK out of the loop and not waist time.
+    WeDidNotSwap = true;
+    for (let j = 0; j > j - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        WedidNotSwap = false;
+      }
+    }
+    if (DidWeSwap) break;
+  }
+
+  return arr;
+}
+
+console.log(OptimalBubbleSort(list));
