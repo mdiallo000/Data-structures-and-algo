feb9e6311b3edf129a44a4228f0a2f4d3d6802ad
mdiallo98 <mamadou1237@gmail.com>
2022-02-24 22:52:06 -0500

    added reverse string qestion

--- BubbleSort.js
+++ BubbleSort.js
@@ -16,26 +16,26 @@
 // ==========================================================================================================
 
 let list = [23, 2, 45, 64, 231, 31];
-function BubbleSort(arr) {
-  // =================================================================================================
-  //? What is the point the point of bubble sort
-  //* Bubble Sort is a sorting that cause the biggest number to get pushed at the back of the list
-  //* The result is a list where the items are sorted from least to greatest
-  //? How does this get achieved?
-  //*  So we compare the numbers as we go check each time if the previous number is greater than the next number
-  //*  If it is we swap.
-  // =================================================================================================
+// function BubbleSort(arr) {
+//   // =================================================================================================
+//   //? What is the point the point of bubble sort
+//   //* Bubble Sort is a sorting that cause the biggest number to get pushed at the back of the list
+//   //* The result is a list where the items are sorted from least to greatest
+//   //? How does this get achieved?
+//   //*  So we compare the numbers as we go check each time if the previous number is greater than the next number
+//   //*  If it is we swap.
+//   // =================================================================================================
 
-  for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < i - 1; j++)
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-  }
-  return arr;
-}
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++)
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//   }
+//   return arr;
+// }
 
 //*  More optimized way of doing Bubble sort is to include a mechanism that will check wheter or not we have a made a swap, if we have then it remains true, if we didnt make a previous swap then we need to brake out of our loop rather than continously looping for no reason. This variable essentially helps us stop the looop if we already finished sorting.
 
@@ -44,20 +44,21 @@ function OptimalBubbleSort(arr) {
   for (let i = arr.length; i > 0; i--) {
     // ** at the start of the loop, DidWeSwap will start true, if we do complete a swap we turn the variable to false so that once it reaches the if condition then we wont break out. But if we fail to swap then, once we reach if(DidWeSwap) it will be true and we can breaK out of the loop and not waist time.
     WeDidNotSwap = true;
-    for (let j = 0; j > j - 1; j++) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
         WedidNotSwap = false;
       }
+
+      if (DidWeSwap) break;
     }
-    if (DidWeSwap) break;
   }
 
   return arr;
 }
 
 // * The Big O of Bubble Sort is N2 because of the double loops most of the time. You may have some edge case were is O(N) for something like this [5,1,2,3,4], since we will only compare once and then break.
-
-console.log(OptimalBubbleSort(list));
+let list2 = [23, 443, 223, 122, 34, 5, 4, 54, 56, 78, 54];
+console.log(OptimalBubbleSort(list2));
--- SelectionSort.js
+++ SelectionSort.js
@@ -1,28 +1,20 @@
-
-function Selectionsort( arr){
-    let N = arr.length;
-    for(let i=0; i<N; i++){
-
-        let minNumber= i
-        for(let j =i+1; j<N; j++){
-            if(arr[j] < arr[minNumber]){
-                minNumber= j
-            }
-            if(minNumber! ==i){
-                let temp = arr[i];
-                arr[i]= arr[minNumber]
-                arr[minNumber] = temp;
-            }
-
-
-
-        }
-
-
-
-
+function Selectionsort(arr) {
+  let N = arr.length;
+  for (let i = 0; i < N; i++) {
+    let minNumber = i;
+    for (let j = i + 1; j < N; j++) {
+      if (arr[j] < arr[minNumber]) {
+        minNumber = j;
+      }
     }
 
-    return arr;
+    if (minNumber !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minNumber];
+      arr[minNumber] = temp;
+    }
+  }
 
+  return arr;
 }
+
