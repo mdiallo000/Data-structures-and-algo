2eec1e65f32bfc7e224cd2a109397404046f0f8d
mdiallo98 <mamadou1237@gmail.com>
2022-02-28 23:16:46 -0500

    Insertion Sort

--- SortingAlgos/SelectionSort.js
+++ SortingAlgos/SelectionSort.js
@@ -18,3 +18,25 @@ function Selectionsort(arr) {
   return arr;
 }
 
+const SelecSort = (arr) => {
+  let min;
+
+  for (let i = 0; i < arr.length; i++) {
+    min = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] < arr[j]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+  }
+
+  return arr;
+};
+
+console.log(SelecSort());
