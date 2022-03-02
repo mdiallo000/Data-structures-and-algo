fb910734f060e0af1ebc0dff4baca67d3e397a39
mdiallo98 <mamadou1237@gmail.com>
2022-03-01 23:44:18 -0500

    MergeSort

--- SortingAlgos/InsertionSort.js
+++ SortingAlgos/InsertionSort.js
@@ -16,3 +16,18 @@ const InsertSort = (arr) => {
 // let numser = [1,34,56,32,45]
 let numbers = [25, 5, 13, 17, 45];
 console.log(InsertSort(numbers));
+
+const Insert = (arr) => {
+  for (let i = 1; i <= arr.length - 1; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+
+  return arr;
+};
+console.log(SelectionSort(numbers));
