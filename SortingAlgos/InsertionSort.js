2fb9f6da28d2e84164869b191052e814a164d65e
mdiallo98 <mamadou1237@gmail.com>
2022-03-01 22:36:39 -0500

    InsertionSort

--- SortingAlgos/InsertionSort.js
+++ SortingAlgos/InsertionSort.js
@@ -1,13 +1,14 @@
 const InsertSort = (arr) => {
   for (let i = 1; i < arr.length - 1; i++) {
     let currentVal = arr[i];
-    for (let j = currentVal; j > 0; j++) {
-      if (currentVal < arr[j]) {
-        let temp = currentVal;
-        currentVal = arr[j];
-        arr[j] = temp;
-      }
+    let j = i - 1;
+    while (j >= 0 && arr[j] > currentVal) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = currentVal;
   }
   return arr;
 };
+// let numser = [1,34,56,32,45]
+console.log(InsertSort([34, 23, 45, 67, 32, 45, 67]));
