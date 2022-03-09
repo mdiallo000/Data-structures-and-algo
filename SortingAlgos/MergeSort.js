901b503af8acbc7198628a44cc8c2b15db9ffc25
mdiallo98 <mamadou1237@gmail.com>
2022-03-09 02:17:12 -0500

    merge two arrays

--- SortingAlgos/MergeSort.js
+++ SortingAlgos/MergeSort.js
@@ -1,18 +1,16 @@
-// ** Whta is merge Sort
+// ? What is merge Sort
+// ** Merge Sort uses the Divide and conquer technique. It essentially brakes down a main unsorted array into smaller pieces that are sorted, and then it combines the mini sorted array into the original size.
 
-const SeletionSort = (arr) => {
-  // ** the goal is to establish some type of min value, compare that min value to every subsquent element in the list, when we encounter a lower value than our og min then we update. Once we are done we can take that min value and bring it to the start of the list
+const MergeSort = (arr) => {};
 
-  for (let i = 0; i <= arr.length - 1; i++) {
-    let min = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > min) {
-      arr[j + 1] = arr[j];
-      j--;
+const MereArrays = (arr1, arr2) => {
+  const newArr = [];
+  // **  [38,42,57]      [34,56,64]
+  let i = 0;
+  let j = 0;
+  while (j <= arr1.length - 1 && i <= arr2.length - 1) {
+    if (arr1[j] < arr2[i]) {
+      newArr.push(arr1[j]);
     }
-    arr[j + 1] = min;
   }
-  return arr;
 };
-let nums = [23, 345, 2, 12, 334, 32, 5, 6, 77, 7, 0, 23];
-console.log(SeletionSort(nums));
