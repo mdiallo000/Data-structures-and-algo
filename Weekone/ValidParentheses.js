35627e2ebcf6eec7c728a6275ee17561fc9da9e4
mdiallo98 <mamadou1237@gmail.com>
2022-03-12 04:14:59 -0500

    has map 1

---
+++ Weekone/ValidParentheses.js
@@ -0,0 +1,18 @@
+class HashTable {
+  constructor(size = 53) {
+    this.KeyMap = new Array(size);
+  }
+  hash (key){
+    let total = 0;
+    let wired_Prime  =31 
+    for (let i =0; i <Math.min(key.length, 100); i++){
+        let char = key[i]
+        let value = char.charCodeAt(0) =96
+        total = (total* wired_Prime + value) % this.KeyMap.length 
+
+    }
+
+    return total
+
+  }
+}
