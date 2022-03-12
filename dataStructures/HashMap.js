9e79f13d832db046681d071e2038b624641cd13e
mdiallo98 <mamadou1237@gmail.com>
2022-03-12 05:09:38 -0500

    has map 1.o

--- dataStructures/HashMap.js
+++ dataStructures/HashMap.js
@@ -1,14 +1,26 @@
-const FindHighValue = (arr) => {
-  let newMax = 0;
+class HashTable {
+  constructor(size = 53) {
+    this.KeyMap = new Array(size);
+  }
+  hash (key){
+    let total = 0;
+    let wired_Prime  =31 
+    for (let i =0; i <Math.min(key.length, 100); i++){
+        let char = key[i]
+        let value = char.charCodeAt(0) =96
+        total = (total* wired_Prime + value) % this.KeyMap.length 
 
-  for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] > newMax) {
-      newMax = arr[i];
     }
+
+    return total
+
   }
 
-  return newMax;
-};
+  Set (key, value){
 
-let nums = [1, 8, 98, 700, 45, 12, 56, 452, 45, 23];
-console.log(FindHighValue(nums));
+    let hashedkey = this.hash(key)
+    
+
+  }
+}
+
