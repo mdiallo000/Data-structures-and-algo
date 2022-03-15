2dfd30416d5ec2f423a9c3dc6a45e10f46aec215
mdiallo98 <mamadou1237@gmail.com>
2022-03-15 19:44:52 -0400

    hash Map 3.0

--- dataStructures/HashMap.js
+++ dataStructures/HashMap.js
@@ -1,31 +1,63 @@
 class HashTable {
   constructor(size = 53) {
-    this.KeyMap = new Array(size);
+    this.KeyMap = new Array((size = 53));
   }
-  hash (key){
+  _hash(key) {
     let total = 0;
-    let wired_Prime  =31 
-    for (let i =0; i <Math.min(key.length, 100); i++){
-        let char = key[i]
-        let value = char.charCodeAt(0) =96
-        total = (total* wired_Prime + value) % this.KeyMap.length 
-
+    let wired_Prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * wired_Prime + value) % this.KeyMap.length;
     }
 
-    return total
-
+    return total;
   }
 
-  Set (key, value){
-
-    let index = this.hash(key)
-     if(!this.KeyMap[index]){
-      this.KeyMap[index] = []
-     }
-     this.KeyMap[index].push([key, value])       
+  Set(key, value) {
+    let index = this._hash(key);
+    if (!this.KeyMap[index]) {
+      this.KeyMap[index] = [];
+    }
+    this.KeyMap[index].push([key, value]);
+  }
+  Get(key) {
+    let index = this._hash(key);
+    if (this.KeyMap[index]) {
+      if (this.KeyMap[index][i][0] === key) {
+        return this.KeyMap[index][i][1];
+      }
+    }
+    return undefined;
+  }
+  Values() {
+    let valueArr = [];
+    for (let i = 0; i < this.KeyMap.length; i++) {
+      if (this.KeyMap[i]) {
+        for (let j = 0; j < this.KeyMap[i].length; j++) {
+          if (!valueArr.includes(thisKeyMap[i][j][1])) {
+            valueArr.push(thisKeyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valueArr;
+  }
+  keys() {
+    let KeysArr = [];
+    for (let i = 0; i < this.KeyMap.length; i++) {
+      if (this.KeyMap[i]) {
+        for (let j = 0; j < this.KeyMap[i].length; j++) {
+          if (!valueArr.includes(thisKeyMap[i][j][0])) {
+            KeysArr.push(thisKeyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return KeysArr;
   }
 }
 
-let newHash = new HashTable()
+let newHash = new HashTable();
 
-newHash.Set()
+newHash.Set();
