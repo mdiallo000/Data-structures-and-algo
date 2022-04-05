e62a8cff79c326003aaa83ea23fbf863673eb615
mdiallo98 <mamadou1237@gmail.com>
2022-04-05 07:00:11 -0400

    BST

--- dataStructures/BinarySearchTree.js
+++ dataStructures/BinarySearchTree.js
@@ -10,9 +10,18 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  Insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    }
+    if (newNode.value > this.root.value) {
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
 tree.root = new Node('I am the root');
 tree.root.left = new Node(4);
 tree.root.right = new Node(21);
+console.log(tree);
