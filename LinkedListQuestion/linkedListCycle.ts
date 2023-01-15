8ac8302126f7cd76c98bad300d800023156c6b6e
mdiallo98 <mamadou1237@gmail.com>
2023-01-14 23:07:57 -0500

    linkedListCycle

---
+++ LinkedListQuestion/linkedListCycle.ts
@@ -0,0 +1,3 @@
+function detectCycle(head: ListNode | null): ListNode | null {
+    //  okay at least i know i am going to need to use the two pointer technique considering we have a linked list and we need to detect where the cycle begins 
+};
