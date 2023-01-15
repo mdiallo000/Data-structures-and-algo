287793f0714ab5f21394f834c4c39e85cb995f5f
mdiallo98 <mamadou1237@gmail.com>
2023-01-14 23:08:48 -0500

    linkedListCycle

--- LinkedListQuestion/linkedListCycle.ts
+++ LinkedListQuestion/linkedListCycle.ts
@@ -1,3 +1,14 @@
+ class ListNode {
+         val: number
+         next: ListNode | null
+         constructor(val?: number, next?: ListNode | null) {
+             this.val = (val===undefined ? 0 : val)
+             this.next = (next===undefined ? null : next)
+         }
+}
+
+
+
 function detectCycle(head: ListNode | null): ListNode | null {
     //  okay at least i know i am going to need to use the two pointer technique considering we have a linked list and we need to detect where the cycle begins 
 };
