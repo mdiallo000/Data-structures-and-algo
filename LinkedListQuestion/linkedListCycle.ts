24bea04b4bd479d0f8dcc7e6065c68bcd65fec5c
mdiallo98 <mamadou1237@gmail.com>
2023-01-14 23:20:26 -0500

    linkedListCycle

--- LinkedListQuestion/linkedListCycle.ts
+++ LinkedListQuestion/linkedListCycle.ts
@@ -13,6 +13,18 @@ function detectCycle(head: ListNode | null): ListNode | null {
     //  okay at least i know i am going to need to use the two pointer technique considering we have a linked list and we need to detect where the cycle begins 
     //  from memory i reconize that we will need to run the cycle detection  function twice, once to determine where the two will meet and then a second around where we start a new pointer at the begining of the list and then another pointer where the cycle began, then wherever these two meet is where the cycle has actually began 
 
-    // steps 
+    // steps for the solution:
+        //? we will have a helper function that will return to us where the two pointers met, if there is no cyle we will return Null and use that for an edge case. 2 we will then create a new pointer that will start at the head of the linked list and we will check whether or not the two will meet once more */
 
 };
+
+
+function findIntersection(head: ListNode): ListNode{
+        
+    let slow =  head 
+    let fast =  head 
+
+
+
+
+}
