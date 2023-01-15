c95163573221d04c4e351dbb45c2c259189b958b
mdiallo98 <mamadou1237@gmail.com>
2023-01-14 23:14:54 -0500

    linkedListCycle

--- LinkedListQuestion/linkedListCycle.ts
+++ LinkedListQuestion/linkedListCycle.ts
@@ -11,6 +11,8 @@
 
 function detectCycle(head: ListNode | null): ListNode | null {
     //  okay at least i know i am going to need to use the two pointer technique considering we have a linked list and we need to detect where the cycle begins 
+    //  from memory i reconize that we will need to run the cycle detection  function twice, once to determine where the two will meet and then a second around where we start a new pointer at the begining of the list and then another pointer where the cycle began, then wherever these two meet is where the cycle has actually began 
 
-    
+    // steps 
+
 };
