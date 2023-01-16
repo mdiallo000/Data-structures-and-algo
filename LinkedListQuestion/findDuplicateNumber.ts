5d91cf6afaa29665e269d6e3a492dd8493a91c2e
mdiallo98 <mamadou1237@gmail.com>
2023-01-15 22:36:41 -0500

    findDuplicateNumber

--- LinkedListQuestion/findDuplicateNumber.ts
+++ LinkedListQuestion/findDuplicateNumber.ts
@@ -1,6 +1,12 @@
 function findDuplicate(nums: number[]): number {
 //  we are meant to return whathever the duplicate value is withing the linked_list 
 //  An obvious way of solving this is to use a set and add all the nodes that we see inside the set and when we encounter the duplicate value we can then return it. This will the starting point and we can potentially improve on it eventually 
-    let net=  new Set()
+        let net=  new Set()
+        
+        for(let curr = 0; curr < nums.length; curr++){
+            
+        }
+
+
 
 };
